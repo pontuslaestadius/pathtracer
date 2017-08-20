496c9eb144245e19ce9e99ae4d79bdc8b6ecd333
Pontus Laestadius <guslaepo@student.gu.se>
2017-08-20 15:33:09 +0200

    fixes locations to be relative

--- target/debug/libpathfinder.d
+++ target/debug/libpathfinder.d
@@ -1,1 +1,1 @@
-/home/pontus/RustProjects/pathfinder/target/debug/libpathfinder.rlib: /home/pontus/RustProjects/pathfinder/src/lib.rs
+/home/pontus/RustProjects/pathfinder/target/debug/libpathfinder.rlib: /home/pontus/RustProjects/pathfinder/src/avocado.rs /home/pontus/RustProjects/pathfinder/src/lib.rs
