6b8454d76e58724d525e22b241219c1367343950
Pontus Laestadius <pontus.laestadius@gmail.com>
2017-11-12 08:32:41 +0100

    removed warnings

--- src/pathfinder/mod.rs
+++ src/pathfinder/mod.rs
@@ -4,8 +4,10 @@ pub mod tools;
 pub mod network;
 
 
+/*
 use pathfinder::node::Node;
 
 fn shortest_path(list: &[Node], from: &Node, to: &Node) {
     // TODO Implement when everything else is done.
 }
+*/
