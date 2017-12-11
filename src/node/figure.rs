17e2ddd42b82dd7c8bd865dd0979e13d36f72d75
Pontus Laestadius <pontus.laestadius@gmail.com>
2017-12-12 00:07:52 +0100

    merges

--- src/node/figure.rs
+++ src/node/figure.rs
@@ -2,7 +2,6 @@
 use super::*;
 use super::coordinates::Coordinate;
 
-
 pub fn rectangle(start_point: &Coordinate, width: i16, height: i16) -> Vec<Node> {
     rectangle_precise(
         start_point.x,
@@ -17,8 +16,53 @@ pub fn rectangle(start_point: &Coordinate, width: i16, height: i16) -> Vec<Node>
 fn rectangle_precise(x1: i16, y1: i16, x2: i16, y2: i16) -> Vec<Node> {
     vec![
         Node::new(String::new(), Coordinate::new(x1, y1)),
-        Node::new(String::new(), Coordinate::new(x1, y2)),
         Node::new(String::new(), Coordinate::new(x2, y1)),
+        Node::new(String::new(), Coordinate::new(x1, y2)),
         Node::new(String::new(), Coordinate::new(x2, y2))
     ]
+}
+
+pub fn cube(start_point: &Coordinate, width: i16, height: i16, depth_height: i16, depth_width: i16) -> Vec<Node> {
+
+    let rem =
+        if depth_width < 0 {
+            if depth_height < 0 {
+                3
+            } else {
+                1
+            }
+    } else {
+        if depth_height < 0 {
+            2
+        } else {
+            0
+        }
+    };
+
+
+    cube_precise(
+        start_point.x,
+        start_point.y,
+        start_point.x + width,
+        start_point.y + height,
+        depth_height,
+        depth_width,
+        rem
+    )
+}
+
+fn cube_precise(x1: i16, y1: i16, x2: i16, y2: i16, dh: i16, dw: i16, rem: usize) -> Vec<Node> {
+    let mut first = rectangle_precise(x1, y1, x2, y2);
+    let mut second = rectangle_precise(x1+dw, y1+dh, x2+dw, y2+dh);
+
+    second.remove(rem);
+    first.append(&mut second);
+
+    first
+}
+
+
+/// Given a list of nodes, will return all pixels inside the formation they have.
+fn get_pixels_in_area(list: &[Node]) -> Vec<Coordinate> {
+    Vec::new()
 }
