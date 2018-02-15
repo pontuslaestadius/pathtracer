57982c51f091e1572ff353cb65b95bda1d6fcbbd
Pontus <pontus.laestadius@gmail.com>
2018-02-15 21:22:04 +0100

    adds converter struct

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -29,48 +29,67 @@ pub fn convert_file<'a>(path: &str, lambda: &Fn(&str) -> bool) -> (Vec<Group>, V
 }
 
 pub fn convert<'a>(content: String, lambda: &Fn(&str) -> bool) -> (Vec<Group>, Vec<Link<'a>>) {
-    let mut groups: Vec<Group> = Vec::new();
-    let links: Vec<Link> = Vec::new();
-    let lines = content.split("\n");
 
-    // Stores the hashed array position rem.
-    let size: usize = 10000;
-    let size_u64: u64 = size as u64;
-    let radius = (size/20) as u32;
-    let node_range = 100;
+    let cct = CustomConverter {
+        split: '\n',
+        node_range: 100,
+        radius: 50,
+        size: 1000,
+        lambda_tag: &lambda,
+    };
+
+    convert_inner(content, cct)
+}
+
+
+pub struct CustomConverter<'a> {
+    split: char,
+    node_range: u32,
+    radius: u32,
+    size: u64,
+    lambda_tag: &'a Fn(&str) -> bool,
+    //lambda_group: &'a Fn(&str) -> bool,
+}
+
+// Heavily customizable.
+pub fn convert_inner<'a>(content: String, cct: CustomConverter) -> (Vec<Group>, Vec<Link<'a>>) {
+    let mut groups: Vec<Group> = Vec::new();
+
+    let lines = content.split(cct.split);
 
     // Check if a group matches the same.
+    // Stores the hashed array position rem.
     let coordinates = Coordinate::new(0, 0);
-    let mut groups_boolean_array: [bool; 10000] = [false; 10000]; // TODO improve.
+    let mut groups_boolean_array: [bool; 1000] = [false; 1000]; // TODO improve.
 
     for line in lines {
         // Ignore empty lines.
         if line == "" {continue};
 
         // Pick up tagged lines.
-        if lambda(line) {
+        if (cct.lambda_tag)(line) {
 
             // Hashes the input value for faster comparison.
             let hashed_line = calculate_hash(&line);
 
             // Checks the boolean array position for the groups existence.
-            if groups_boolean_array[(hashed_line % size_u64) as usize] {
+            if groups_boolean_array[(hashed_line % cct.size) as usize] {
 
                 for old in &mut groups.iter_mut() {
                     // If it does not match existing tag.
                     if old.hash != hashed_line {continue};
-                    let _ = old.new_node_min_auto(String::new(), node_range);
+                    let _ = old.new_node_min_auto(String::new(), cct.node_range);
                     break;
                 }
 
                 // Creates a new group because one did not exist.
             } else {
                 // Sets the group to exists in the boolean array.
-                groups_boolean_array[(hashed_line % size_u64) as usize] = true;
+                groups_boolean_array[(hashed_line % cct.size) as usize] = true;
                 // Produce a new group.
                 let mut group = Group::new(
                     &line,
-                    gen_radius(&coordinates, 0, radius*2),
+                    gen_radius(&coordinates, 0, cct.radius*2),
                     util::gen_rgba_reliable(calculate_hash(&line)),
                     None
                 );
@@ -84,7 +103,7 @@ pub fn convert<'a>(content: String, lambda: &Fn(&str) -> bool) -> (Vec<Group>, V
 
     }
 
-    (groups, links)
+    (groups, Vec::new())
 }
 
 pub fn calculate_hash<T: Hash>(t: &T) -> u64 {
