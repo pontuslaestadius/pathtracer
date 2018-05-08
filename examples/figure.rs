f539a43b56478c1e0f7c8f81e0d98d0895707f4e
PontusLaestadius <pontus.laestadius@gmail.com>
2018-05-08 11:34:42 +0200

    Fixes linking borrow issue, still a problem for A-B-C. Improved code and added doc test

--- examples/figure.rs
+++ examples/figure.rs
@@ -1,30 +1,3 @@
-/*
-    Store all nodes within a file.
-        -> inside a subdirectory
-       (->) If to many nodes exist use a secondary file.
-
-
-    ( Node generation should be a secondary project, as they are too complex to hand write.)
-    A node contains:
-        -> name: &str
-        -> connecting nodes
-            -> distance to them
-            -> ways of transportation with times.
-       (->) absolute x and y positions.
-
-
-
-    It will use multiple algorithms to find the shortest and fastest path using whatever
-    transportation the user has requested. (any transportation by default)
--
-    Djikstras, A-pointer
-        (->) including alternative paths.
-
-    The user will get a summery of the trip,
-    The user can request a generated map of traveling between the nodes to the destination.
-
- */
-
 extern crate pathfinder;
 extern crate rand;
 extern crate image;
@@ -49,7 +22,6 @@ fn main() {
     //let links = sequentially_link_nodes(&nodes);
 
     let path = std::path::Path::new("figure.png");
-
 
     let mut map = Map::new();
     map = map
--- examples/random.rs
+++ examples/random.rs
@@ -1,89 +1,75 @@
-/*
-    Store all nodes within a file.
-        -> inside a subdirectory
-       (->) If to many nodes exist use a secondary file.
-
-
-    ( Node generation should be a secondary project, as they are too complex to hand write.)
-    A node contains:
-        -> name: &str
-        -> connecting nodes
-            -> distance to them
-            -> ways of transportation with times.
-       (->) absolute x and y positions.
-
-
-
-    It will use multiple algorithms to find the shortest and fastest path using whatever
-    transportation the user has requested. (any transportation by default)
--
-    Djikstras, A-pointer
-        (->) including alternative paths.
-
-    The user will get a summery of the trip,
-    The user can request a generated map of traveling between the nodes to the destination.
-
- */
-
 extern crate pathfinder;
 extern crate rand;
 extern crate image;
-use pathfinder::map;
 use pathfinder::*;
 use image::Rgba;
 
 /**
-Creates two groups filled with a set of children randomly position withined a radius.
+Creates two groups filled with a set of children randomly position within a radius.
 The two groups are linked together.
 
 **/
 
+
 fn main() {
+
     // Coordinates for the groups.
     let group1_coordinates = Coordinate::new(0,0);
     let group2_coordinates = Coordinate::new(250,250);
+    let group3_coordinates = Coordinate::new(150,20);
 
     // Names for the groups.
     let group1_name = "john doe".to_string();
     let group2_name = "jane doe".to_string();
+    let group3_name = "sane moo".to_string();
 
     // Colors for the groups.
     let group1_color = Rgba {data: [250, 20, 20, 255]};
     let group2_color = Rgba {data: [20, 20, 250, 255]};
+    let group3_color = Rgba {data: [20, 250, 20, 255]};
 
-    // Group 1.
+
+
+    let mut group2: Group<Square> = Group::new(
+        &group2_name,
+        group2_coordinates,
+    );
+
+    let mut group3 = Group::new(
+        &group3_name,
+        group3_coordinates,
+    );
+
     let mut group1 = Group::new(
         &group1_name,
         group1_coordinates,
     );
 
-    // Group 2.
-    let mut group2 = Group::new(
-        &group2_name,
-        group2_coordinates,
-    );
+    group1.settings.color = group1_color;
+    group2.settings.color = group2_color;
+    group3.settings.color = group3_color;
+
+    group1.settings.radius = Some(50);
+    group2.settings.radius = Some(70);
+    group3.settings.radius = Some(40);
+
+    // List of groups.
+    //let mut groups: Vec<Group<Square>> = vec!(&group1, &group2, &group3);
+    let children = 800;
+
+    // Add children.
+    map::network::add_children(&mut group1, children);
+    map::network::add_children(&mut group2, children);
+    map::network::add_children(&mut group3, children);
 
     //group1.link(&group2);
 
-    group1.settings.color = group1_color;
-    group2.settings.color = group2_color;
-
-    // List of groups.
-    let mut groups: Vec<Group<Square>> = vec!(group1, group2);
-
-    let children = 200;
-
-    // Add children.
-    for group in groups.iter_mut() {
-        map::network::add_children(group, children);
-    }
-
     // Where and what to call the file.
     let path= std::path::Path::new("random.png");
-
     let mut map = Map::new();
+
     map = map
-        .map(&groups);
+        .map(&[group1, group2, group3]);
 
     let _ = map.image.unwrap().save(&path);
 }
