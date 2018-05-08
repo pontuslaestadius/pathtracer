427d9ece9e056948979d2908e76c7185d0f6c647
PontusLaestadius <pontus.laestadius@gmail.com>
2018-05-08 11:34:42 +0200

    Fixes linking borrow issue, still a problem for A-B-C. Improved code and added doc test

--- src/tools/constant.rs
+++
@@ -1,5 +0,0 @@
-pub static NODEPATH: &str = "resources/nodes.txt";
-pub static LINKPATH: &str = "resources/links.txt";
-pub static NAMEPATH: &str = "resources/nodenames.txt";
-pub static DEBUGMODE: bool = true;
-pub static CONFIRMA: bool = true;
