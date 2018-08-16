c912198e74f6f35ce48bc48a260a1ac989bf6e69
Pontus Laestadius <pontus.laestadius@gmail.com>
2018-08-16 20:23:14 +0200

    [#12] Solves Link referencing issue and refactoring

--- examples/random.rs
+++ examples/random.rs
@@ -8,8 +8,8 @@ use image::Rgba;
     Creates three groups filled with children randomly position within it's radius.
 */
 
-fn main() { 
-    let children = 800;
+fn main() {
+    let children = 600;
     let color = [
         Rgba {data: [250, 20, 20, 255]},
         Rgba {data: [20, 20, 250, 255]},
@@ -17,11 +17,11 @@ fn main() {
     ];
     let radius = [
         Some(50),
-        Some(70),
-        Some(40)
+        Some(60),
+        Some(65)
     ];
 
-    let mut groups = Group::from_list(&[(0,0),(250,250),(150,20)]);
+    let mut groups = Group::from_list(&[(0,0),(240,20),(115,40)]);
 
     for (i, ref mut group) in groups.iter_mut().enumerate() {
         group.settings.radius = radius[i];
