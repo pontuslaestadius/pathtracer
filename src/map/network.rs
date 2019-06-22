07ba0db870f8ba626d857a58d94eacd487cab53e
Pontus Laestadius <pontus.laestadius@gmail.com>
2019-06-22 13:45:30 +0200

    Improves path algorithm

--- src/map/network.rs
+++ src/map/network.rs
@@ -6,6 +6,14 @@ use super::*;
 use std::io::{self, Error, ErrorKind};
 
 /**
+Weighted Node
+*/
+struct WNodes {
+    nodes: Vec<Node>,
+    weight: u32,
+}
+
+/**
  Paths between two different points that are connected.
 
 
@@ -59,47 +67,88 @@ pub fn path_shortest_leg<'a>(
     start: Node,
     goal: Node,
 ) -> io::Result<Vec<Node>> {
-    let format = |mut from: Vec<Node>, link: &HL, acc: u32| -> (u32, Vec<Node>) {
-        let node_opt = network.hash_map[link.t as usize % consts::NETWORK_REM];
-        let node = node_opt.unwrap_or_else(|| {
-            panic!(
-                "Node missing in network. From: {:?}, Link:
-        {:?}",
-                from, link
-            )
-        });
-        let dis = coordinate::distance(from[0].geo, node.geo);
-        from.insert(0, node);
-        (acc + dis, from)
+    // Create a new Branch-off path.
+    let format = |mut nodes: Vec<Node>, link: &HL, acc: u32| -> WNodes {
+        let node = network.hash_map[link.t as usize % consts::NETWORK_REM].unwrap();
+        let weight = acc + coordinate::distance(nodes.first().unwrap().geo, node.geo);
+        nodes.insert(0, node);
+        WNodes { weight, nodes }
     };
 
-    let mut queue: Vec<(u32, Vec<Node>)> =
-        start
+    // Create the queue from connected links.
+    let mut queue: Vec<WNodes> = start
+        .links()
+        .iter()
+        .filter(|x| x.is_connected())
+        .map(|x| format(vec![start], x, 0))
+        .collect::<Vec<_>>();
+
+    while !queue.is_empty() {
+        // Sort the queue based on weight.
+        queue.sort_by_key(|wn| wn.weight);
+
+        // Pull the closest path from the Queue.
+        let wnodes = queue.remove(0);
+
+        // Get the current Node in the closest path.
+        let current = wnodes.nodes.first().unwrap();
+
+        // End if we are at the goal.
+        if current.hash == goal.hash {
+            return Ok(wnodes.nodes);
+        }
+
+        // Push new paths to the queue.
+        let _ = current
             .links()
             .iter()
             .filter(|x| x.is_connected())
-            .fold(vec![], |mut acc, x| {
-                acc.push(format(vec![start], x, 0));
-                acc
-            });
-
-    while !queue.is_empty() {
-        queue.sort_by_key(|k| k.0);
-        let (dis, path) = queue.remove(0);
-        let current = path[0];
-
-        if current.hash == goal.hash {
-            return Ok(path);
-        }
-
-        for link in current.links().iter() {
-            if link.is_connected() {
-                queue.push(format(path.clone(), link, dis));
-            }
-        }
+            .map(|x| queue.push(format(wnodes.nodes.clone(), x, wnodes.weight)))
+            .collect::<Vec<_>>();
     }
+
+    // If we run out of items in the Queue, and we have not reacted
+    // the goal, the path is invalid. And does not exist.
     Err(Error::new(ErrorKind::Other, "not a valid path"))
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn simple_network_shortest_leg() {
+        let nodes = Node::from_list(&[(0, 0), (10, 10), (20, 20), (30, 30)]);
+        let nodes = Node::linked_list(nodes);
+        let net = Network::new(nodes);
+        let path = path(&net, "D", "A", &path_shortest_leg).unwrap();
+        assert_eq!(path.len(), 4);
+    }
+
+    #[test]
+    fn simple_network() {
+        let nodes = Node::from_list(&[(0, 0), (10, 10), (20, 20), (30, 30)]);
+        let nodes = Node::linked_list(nodes);
+        let path = Network::new(nodes).path("A", "D").unwrap();
+        assert_eq!(path.len(), 4);
+    }
+
+    #[test]
+    fn simple_networks_have_same_path() {
+        let nodes = Node::from_list(&[(0, 0), (10, 10), (20, 20), (30, 30)]);
+        let nodes = Node::linked_list(nodes);
+
+        let net = Network::new(nodes);
+        let mut path_sl = path(&net, "D", "A", &path_shortest_leg).unwrap();
+        let path = net.path("A", "D").unwrap();
+        path_sl.reverse();
+
+        let f = |&p: &Node| p.geo;
+        let v1 = path.iter().map(f).collect::<Vec<_>>();
+        let v2 = path_sl.iter().map(f).collect::<Vec<_>>();
+
+        assert_eq!(v1, v2);
+    }
+
+}
